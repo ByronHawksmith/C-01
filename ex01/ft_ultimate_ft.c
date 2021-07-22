6d2408ffc04bee0a78f9111e40c5e5d68475988a
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-23 08:20:32 +0930

    Update .gitignore, complete ex01

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+/*
+void	ft_ultimate_ft(int *********nbr)
+{
+	int var;
+	char lh;
+	char rh;
+
+	*********nbr = 42;
+	var = *********nbr;
+	lh = var / 10 + '0';
+	rh = var % 10 + '0';
+
+	write(1, &lh, 1);
+	write(1, &rh, 1);
+}
+*/
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*
+int	main(void)
+{
+	int var;
+
+	var = 100;
+
+	int    *a;
+	int    **b;
+	int    ***c;
+	int    ****d;
+	int    *****e;
+	int    ******f;
+	int    *******g;
+	int    ********h;
+	int    *********i;
+	a = &var;
+	b = &a;
+	c = &b;
+	d = &c;
+	e = &d;
+	f = &e;
+	g = &f;
+	h = &g;
+	i = &h;
+	ft_ultimate_ft(i);
+	return (0);
+}
+*/
