bead485d83b8e8783386e7d59edad68597231778
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-22 17:13:45 +0930

    Remove main() function from ft_ft.c

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,24 +1,6 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_ft(int *nbr)
 {
-	int	n;
-
 	*nbr = 42;
-	n = *nbr;
-	ft_putchar(n / 10 + '0');
-	ft_putchar(n % 10 + '0');
-}
-
-int	main(void)
-{
-	int	number;
-
-	number = 100;
-	ft_ft(&number);
 }
