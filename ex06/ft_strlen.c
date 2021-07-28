ac76db8ad8232ef1c4a564ab7eab4872319e9254
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-07-28 16:22:01 +0930

    Finish ex06

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,23 @@
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	int len;
+	char	*str = "Test!";
+
+	len = ft_strlen(str);
+	printf("Length of s: %s, is %d", str, len);
+}
+*/
