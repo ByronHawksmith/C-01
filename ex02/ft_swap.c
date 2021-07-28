b4e1e872d090ddbdec99853a0073c6f6eee8ded9
Byron Hawksmith <bhawksmi@F2K026.42adel.org.au>
2021-07-28 16:59:11 +0930

    Update test case ex02

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -9,28 +9,17 @@ void	ft_swap(int *a, int *b)
 
 /*
 #include <stdio.h>
-void	ft_swap(int *a, int *b)
-{
-	int	tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-
-	printf("a: %d\n", *a);
-	printf("b: %d", *b);
-}
-
 int	main(void)
 {
-	int a_val = 4;
-	int b_val = 2;
-	int *a;
-	int *b;
+	int a;
+	int b;
 
-	a = &a_val;
-	b = &b_val;
-
-	ft_swap(a, b);
+	a = 4;
+	b = 2;
+	printf("Before a: %d\n", a);
+	printf("Before b: %d\n", b);
+	ft_swap(&a, &b);
+	printf("After a: %d\n", a);
+	printf("After b: %d\n", b);
 }
 */
